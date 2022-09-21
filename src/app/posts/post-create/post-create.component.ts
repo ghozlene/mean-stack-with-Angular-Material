87082cc67528a8f5a4dc9e6bd8322afff34c1fbe
Achref <medachrefg@gmail.com>
2022-09-21 03:00:13 +0100

    adding Forms

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { Post } from "../post.model";
+
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -13,10 +14,13 @@ export class PostCreateComponent {
   @Output() postCreated = new EventEmitter<Post>();
 
 
-  onAddPost() {
+  onAddPost(form) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.entredTitle,
-      content: this.entredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
