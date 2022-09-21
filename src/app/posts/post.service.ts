617231c0519a056395096349935e9d97df27efd2
Achref <medachrefg@gmail.com>
2022-09-21 15:52:23 +0100

    transforming  Response data

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { Subject } from 'rxjs';
+import { pipe, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 @Injectable({ providedIn: 'root' })
 export class PostService {
 
@@ -13,9 +14,22 @@ export class PostService {
   }
 
   getPost() {
-    this.http.get<{ message: string, posts: Post[]; }>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http.get<{ message: string, posts: any; }>
+      ('http://localhost:3000/api/posts')
+
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+
+      }))
+      .subscribe((TransformPosts) => {
+        console.log(TransformPosts);
+        this.posts = TransformPosts;
         this.postsUpdated.next([...this.posts]);
       }
       );
