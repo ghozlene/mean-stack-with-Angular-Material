cbad6a687fc53e0be43756685e9324dc487295eb
Achref <medachrefg@gmail.com>
2022-09-22 16:24:53 +0100

    organizing our backend Routes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
-const Post = require("./models/post");
+const postsRoutes = require("./routes/posts");
 const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose
@@ -18,60 +18,5 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(cors());
-
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((ceatedPost) => {
-    res
-      .status(201)
-      .json({ message: "data added successfully", postId: ceatedPost._id });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find()
-    .then((doc) => {
-      res.status(200).json({
-        message: "posts fetched with success",
-        posts: doc,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "post ==> not found", posts: posts });
-    }
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({
-      message: "deleted with success",
-    });
-  });
-});
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    console.log(result);
-    res.status(201).json({ message: "updated with success" });
-  });
-});
-
+app.use("/api/posts", postsRoutes);
 module.exports = app;
