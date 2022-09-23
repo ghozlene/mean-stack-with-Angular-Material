6a50b0bfedc8a1d9d405a07b319cfef7aaf8bd1d
Achref <medachrefg@gmail.com>
2022-09-23 01:28:48 +0100

    fetching images on the frontend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
 const postsRoutes = require("./routes/posts");
@@ -18,5 +19,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/images", express.static(path.join("backend/images")));
+
 app.use("/api/posts", postsRoutes);
 module.exports = app;
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -30,20 +30,20 @@ router.post(
   "",
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
-    const url = req.protocol + "//" + req.get("host");
+    const url = req.protocol + "://" + req.get("host");
     const post = new Post({
       title: req.body.title,
       content: req.body.content,
       imagePath: url + "/images/" + req.file.filename,
     });
     post.save().then((createdPost) => {
-      res.status(201).json(
-        { message: "data added successfully" },
-        {
+      res.status(201).json({
+        message: "data added successfully",
+        post: {
           id: createdPost._id,
           ...createdPost,
-        }
-      );
+        },
+      });
     });
   }
 );
